d2bd35d0efa46493a3f69510506f231574d043a5
Paul Otto <potto@ieee.org>
2024-04-05 20:54:14 -0400

    updated to support more command line arguments

--- le_advertise/src/main.rs
+++ le_advertise/src/main.rs
@@ -7,43 +7,60 @@ use tokio::{
     time::sleep,
 };
 
+use std::collections::{BTreeSet};
+use std::str::FromStr;
 use structopt::StructOpt;
+use uuid::Uuid; // Make sure the `uuid` crate is added to your dependencies
 //use std::str::FromStr;
 
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "le_advertise", about = "A command tool to generate BLE advertisements")]
 struct Opt {
-    /// Activate debug mode
-    // short and long flags (-d, --debug) will be deduced from the field's name
-    #[structopt(short, long, help="Show additional information for troubleshooting such as details about the adapters")]
-    debug: bool,
+    /// Activate verbose mode
+    // short and long flags (-v, --verbose) will be deduced from the field's name
+    #[structopt(short, long, help="Show detailed information for troubleshooting, including details about the adapters")]
+    verbose: bool,
  
     // short and long flags (-a, --advertiser) will be deduced from the field's name     
-    #[structopt(short, long, required=true, help="The advertisement address in the form XX:XX:XX:XX:XX:XX  ex: 5C:F3:70:A1:71:0F")]
+    #[structopt(short, long, default_value="", help="The advertisement address in the form XX:XX:XX:XX:XX:XX  ex: 5C:F3:70:A1:71:0F")]
     advertiser: String,
 
     // short and long flags (-u, --uuid-service) will be deduced from the field's name     
     #[structopt(short, long, default_value="", help="an optional uuid service to add to the advertisement. ex: 123e4567-e89b-12d3-a456-426614174000")]
     uuid_service: String,
     
+     /// UUID service to add to the advertisement. ex: 123e4567-e89b-12d3-a456-426614174000
+     #[structopt(short = "u", long, use_delimiter = true, help = "List of service UUIDs separated by commas")]
+     service_uuids: Vec<String>,
+ 
+     /// Local name to be used in the advertising report.
+     #[structopt(short, long, help = "Local name for the advertisement")]
+     local_name: Option<String>,
+ 
+     /// Advertise as general discoverable.
+     #[structopt(long, help = "Advertise as general discoverable")]
+     discoverable: Option<bool>,
+ 
+     /// Duration of the advertisement in seconds.
+     #[structopt(long, help = "Duration of the advertisement in seconds")]
+     duration: Option<u64>,
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> bluer::Result<()> {
 
     let opt = Opt::from_args();
+    let verbose = opt.verbose;
     env_logger::init();
 
-    println!("{:?}", opt);
-
-    let debug_mode = opt.debug;    
-    if debug_mode
-    {
-        println!("{:?}", opt);
-    }
-    let my_address = opt.advertiser;
     let uuid_service = opt.uuid_service;
+
+    let service_uuids: BTreeSet<Uuid> = opt
+    .service_uuids
+    .iter()
+    .filter_map(|s| Uuid::from_str(s).ok())
+    .collect();
 
     let session = bluer::Session::new().await?;
             
@@ -52,22 +69,27 @@ async fn main() -> bluer::Result<()> {
     let adapter_name = adapter_names.first().expect("No Bluetooth adapter present");
     let mut adapter = session.adapter(adapter_name)?;
     for adapter_name in adapter_names {
-        println!("Checking Bluetooth adapter {}:", &adapter_name);
         let adapter_tmp = session.adapter(&adapter_name)?;
         let address = adapter_tmp.address().await?;
-        if  address.to_string() == my_address {
-            adapter =  adapter_tmp;
+
+        if verbose {
+            println!("Checking Bluetooth adapter {}: with an address {}", &adapter_name, address);
+        }        
+
+        if opt.advertiser.is_empty() || address.to_string() == opt.advertiser {
+            adapter = adapter_tmp;
+            if verbose {
+                println!("Using Bluetooth adapter {}", &adapter_name);
+                println!("    Address: {}", address);
+                // Print additional adapter details here as needed
+            }
             break;
         }
     };
-
-    //let adapter_name = adapter_names.first().expect("No Bluetooth adapter present");
-    //let adapter = session.adapter(adapter_name)?;
-    let adapter_name = adapter.name();    
+ 
     adapter.set_powered(true).await?;    
-    if debug_mode
-    {
-        println!("Advertising on Bluetooth adapter {}", &adapter_name);
+    if opt.verbose {
+        println!("Advertising on Bluetooth adapter {}", adapter.name());
         println!("    Address:                    {}", adapter.address().await?);
         println!("    Address type:               {}", adapter.address_type().await?);
         println!("    Friendly name:              {}", adapter.alias().await?);
@@ -75,27 +97,16 @@ async fn main() -> bluer::Result<()> {
         println!("    Modalias:                   {:?}", adapter.modalias().await?);
         println!("    Powered:                    {:?}", adapter.is_powered().await?);        
     }
-
-    let le_advertisement = if uuid_service==""
-    {
-        Advertisement {
-            advertisement_type: bluer::adv::Type::Peripheral,
-            discoverable: Some(true),
-            local_name: Some("le_advertise".to_string()),
-            ..Default::default()
-        }
-    }
-    else
-    {
-       Advertisement {
-            advertisement_type: bluer::adv::Type::Peripheral,
-            service_uuids: vec![uuid_service.parse().unwrap()].into_iter().collect(),
-            discoverable: Some(true),
-            local_name: Some("le_advertise".to_string()),
-            ..Default::default()
-        }
+    let le_advertisement = Advertisement {
+        advertisement_type: bluer::adv::Type::Peripheral,
+        service_uuids: service_uuids,
+        local_name: opt.local_name,
+        discoverable: opt.discoverable,
+        duration: opt.duration.map(Duration::from_secs),
+        ..Default::default()
     };
-    if debug_mode
+
+    if verbose    
     {
         println!("{:?}", &le_advertisement);
     }
@@ -106,7 +117,7 @@ async fn main() -> bluer::Result<()> {
     let mut lines = stdin.lines();
     let _ = lines.next_line().await;
 
-    if debug_mode
+    if verbose
     {
         println!("Removing advertisement");
     }
